3f14d7ad9c76ec4db0195bab61f38988c33e1d74
Wanderson Oliveira da Silva <wanderson.oliveira@senaicni.com.br>
2026-02-13 13:28:07 -0300

    consumindo api de series

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,21 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoAPI = new ConsumoAPI();
+        var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=d97119bc");
+        System.out.println(json);
+    }
 }
