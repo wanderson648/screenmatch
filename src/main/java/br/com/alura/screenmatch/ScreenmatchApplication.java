9649a744991413f37c59efb61daf4ef10461575d
Wanderson Oliveira da Silva <wanderson.oliveira@senaicni.com.br>
2026-02-13 16:37:36 -0300

    trazendo dados da temporada

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSeries;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverterDados;
 import org.slf4j.Logger;
@@ -8,6 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,13 +26,34 @@ public class ScreenmatchApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         var consumoAPI = new ConsumoAPI();
-        log.info("Consumindo API");
-        var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=d97119bc");
-        System.out.println(json);
+        log.info("Consumindo series da API");
+        var jsonSeries = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=d97119bc");
+        System.out.println(jsonSeries);
 
         log.info("Convertendo JSON para DadosSeries");
-        ConverterDados converteDados = new ConverterDados();
-        DadosSeries dados = converteDados.converter(json, DadosSeries.class);
-        System.out.println(dados);
+        ConverterDados converteDadosSeries = new ConverterDados();
+        DadosSeries dadosSeries = converteDadosSeries.converter(jsonSeries, DadosSeries.class);
+        System.out.println(dadosSeries);
+
+        log.info("Consumindo epsodios da API");
+        var jsonEp = consumoAPI.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=d97119bc");
+        System.out.println(jsonEp);
+
+        log.info("Convertendo JSON para DadosEpsodios");
+        ConverterDados converterDadosEp = new ConverterDados();
+        DadosEpisodio dadosEp = converterDadosEp.converter(jsonEp, DadosEpisodio.class);
+        System.out.println(dadosEp);
+
+        System.out.println("=======================================");
+        log.info("Dados das temporadas");
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for(int i = 1; i<= dadosSeries.totalTemporadas(); i++) {
+            jsonEp = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=d97119bc");
+            DadosTemporada dadosTemporada = converterDadosEp.converter(jsonEp, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+
+        }
+        temporadas.forEach(System.out::println);
     }
 }
