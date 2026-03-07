500ed266bae5050079f839540b2ddb6f9a049cfd
wanderson648 <testepedroca@gmail.com>
2026-03-07 19:11:36 -0300

    screenmatch com jpa

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,8 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.principal.Principal;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,15 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-    private static final Logger log = LoggerFactory.getLogger(ScreenmatchApplication.class);
+    @Autowired
+    private SerieRepository repositorio;
 
     public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
     @Override
-    public void run(String... args) {
-        Principal principal = new Principal();
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(repositorio);
         principal.exibeMenu();
     }
 }
--- src/main/java/br/com/alura/screenmatch/service/ConsumoAPI.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsumoAPI.java
@@ -6,23 +6,24 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class ConsumoAPI {
+public class ConsumoApi {
 
     public String obterDados(String endereco) {
-
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = null;
-
         try {
             response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        return response.body();
+        String json = response.body();
+        return json;
     }
 }
